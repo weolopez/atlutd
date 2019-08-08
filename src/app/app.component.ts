ce703687973cdbea153b66fdd7d5a3f3477a6a9d
Mauricio Lopez <weolopez@dw135-199-3-220.dhcpw.sest.att.com>
2019-08-08 07:25:46 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import { AuthProcessService } from 'ngx-auth-firebaseui';
+import { auth } from 'firebase/app';
+
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item { name: string; }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +17,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'atlutd';
+  private itemDoc: AngularFirestoreDocument<Item>;
+  items: Observable<any[]>;
+  public loggedIn = false;
+  public showProviders = false;
+
+  constructor(db: AngularFirestore,
+              public afAuth: AuthProcessService
+    ) {
+      afAuth.afa.user.subscribe(event => {
+        this.loggedIn = (!event) ? false : true;
+      });
+      this.items = db.collection('users').valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule,
+    NgxAuthFirebaseUIModule.forRoot(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
