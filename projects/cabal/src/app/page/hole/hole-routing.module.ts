0dff968909106097a3f4bfb51337b5e901013951
Lopez, Mauricio (ml5174) <ml5174@att.com>
2020-12-17 15:49:33 -0500

    [cms001] fixed defect test

--- projects/cabal/src/app/page/hole/hole-routing.module.ts
+++ projects/cabal/src/app/page/hole/hole-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HoleComponent } from './hole.component';
 
 const routes: Routes = [
-  { path: '', component: HoleComponent }
+  { path: '', component: HoleComponent },
+  { path: ':golfround', component: HoleComponent }
 ];
 
 @NgModule({
--- projects/cabal/src/app/page/hole/hole.component.ts
+++ projects/cabal/src/app/page/hole/hole.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 import { AuthService } from '../../services/auth.service';
 import { Observable, Subject } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 export interface Item { name: string; }
 @Component({
@@ -10,114 +12,20 @@ export interface Item { name: string; }
   templateUrl: './hole.component.html',
   styleUrls: ['./hole.component.scss']
 })
-export class HoleComponent implements OnInit {
+export class HoleComponent {
+  holes;
+  golfcourses;
+  golfround;
+  rounds;  
+  games: any;
+  currentUser: any;
+  user: Observable<any>;
+  roundCollection: any;
+  currentHole: any;
+  golfCourseCollection: any;
+  golfRoundObs: any;
+  filteredUsersObs: Observable<any>;
 
-  holes = [
-    {
-      hole: 1,
-      par: 3,
-      score: 3
-    },
-    {
-      hole: 2,
-      par: 3,
-      score: 3
-    },
-    {
-      hole: 3,
-      par: 3,
-      score: 3
-    },
-    {
-      hole: 4,
-      par: 3,
-      score: 3
-    },
-    {
-      hole: 5,
-      par: 3,
-      score: 3
-    },
-    {
-      hole: 6,
-      par: 3,
-      score: 3
-    },
-    {
-      hole: 7,
-      par: 3,
-      score: 3
-    },
-    {
-      hole: 8,
-      par: 3,
-      score: 3
-    },
-    {
-      hole: 9,
-      par: 3,
-      score: 3
-    },
-    {
-      hole: 10,
-      par: 5,
-      score: 5
-    },
-    {
-      hole: 11,
-      par: 5,
-      score: 5
-    },
-    {
-      hole: 12,
-      par: 5,
-      score: 5
-    },
-    {
-      hole: 13,
-      par: 5,
-      score: 5
-    },
-    {
-      hole: 14,
-      par: 5,
-      score: 5
-    },
-    {
-      hole: 15,
-      par: 5,
-      score: 5
-    },
-    {
-      hole: 16,
-      par: 5,
-      score: 5
-    },
-    {
-      hole: 17,
-      par: 5,
-      score: 5
-    },
-    {
-      hole: 18,
-      par: 5,
-      score: 5
-    }
-  ];
-  golfcourse={
-    holes: this.holes
-  }
-  g = [
-    {
-      hole: this.holes
-    },
-    {
-      hole: this.holes
-    },
-    {
-      hole: this.holes
-    }
-  ]
 
   private itemDoc: AngularFirestoreDocument<Item>;
   public users: Observable<any[]>;
@@ -140,9 +48,177 @@ export class HoleComponent implements OnInit {
   private trigger: Subject<void> = new Subject<void>();
   // switch to next / previous / specific webcam; true/false: forward/backwards, string: deviceId
   private nextWebcam: Subject<boolean | string> = new Subject<boolean | string>();
-  games: any;
-  currentUser: any;
-  user: Observable<any>;
+  usersCollection: any;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private db: AngularFirestore,
+    private auth: AuthService
+  ) {
+    this.usersCollection = db.collection('users');
+    this.roundCollection = db.collection('golfround');
+    this.golfCourseCollection = db.collection('golfcourse');
+
+    this.golfround = this.route.snapshot.paramMap.get('golfround');
+
+    if (this.golfround) {
+      this.golfRoundObs = this.roundCollection.doc(this.golfround).valueChanges();
+    } else {
+      this.golfcourses = this.golfCourseCollection.valueChanges();
+    }
+
+    if (this.golfround) {
+      this.golfRoundObs = this.roundCollection.doc(this.golfround)
+          .valueChanges();
+
+          // .pipe(tap((round: any) => {
+      this.filteredUsersObs = this.usersCollection.valueChanges()
+              .pipe(map((users: Array<any>) => {
+                let u = users.filter(u=> { 
+                    return (u.golfrounds) ? (u.golfrounds[this.golfround]) ? true : false : false;
+                });
+                return u;
+              })); 
+          // })); 
+    } else {
+      this.rounds = this.roundCollection.valueChanges();
+    }
+
+    this.users = this.usersCollection.valueChanges();
+    this.getUser().subscribe(u => { this.currentUser = u });
+
+    WebcamUtil.getAvailableVideoInputs()
+      .then((mediaDevices: MediaDeviceInfo[]) => {
+        this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
+      });
+    db.collection('games').valueChanges().subscribe(g => {
+      this.games = g;
+    });
+  }
+
+  updateProfileImage(currentUser) {
+    currentUser.photoURL = this.webcamImage;
+    this.auth.updateUserData(currentUser).then(this.webcamImage = undefined);
+  }
+  updateDisplayName(currentUser, name) {
+    currentUser.displayName = name;
+    this.auth.updateUserData(currentUser).then(this.webcamImage = undefined);
+  }
+  updateSMS(currentUser, phoneNumber) {
+    currentUser.phoneNumber = phoneNumber;
+    this.auth.updateUserData(currentUser).then();
+  }
+  getUser() {
+    this.user = this.auth.getUser();
+    return this.user;
+  }
+  getKey(seat) {
+    let key = Object.keys(seat)[0];
+    if (!this.games) return;
+    let game = this.games.filter(game => game.id == key);
+    game[0].seat = seat[key];
+    return game;
+  }
+
+  getGame(item) {
+    let gameId = item.key;
+    let isStar = item.value
+    if (!isStar) return;
+    let game = this.games.filter(game => game.id == gameId)
+    return game;
+  }
+  switchUser(id) {
+    this.auth.switchUser(id);
+    this.getUser();
+  }
+  getToday() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    return mm + '/' + dd + '/' + yyyy;
+  }
+  getPar(g) {
+    return g.holes.reduce((sum, h) => sum + h.par, 0);
+  }
+  newRound(g, name) {
+    let round = Object.assign({}, g);
+    round.id = name;
+    round.date = this.getToday();
+    this.roundCollection.doc(name)
+      .set(round);
+  }
+  join(g) {
+    g.members = (g.members) ? g.members : [];
+    if (g.members.includes(this.currentUser.uid)) return;
+    this.user.subscribe(u => {
+      g.members.push(u.uid);
+      u.golfrounds = (u.golfrounds) ? u.golfrounds : {};
+      this.roundCollection.doc(g.id).update(g);
+      if (!u.golfrounds[g.id]) {
+        let round = {
+          golfcourse: g.name,
+          score: { 0:0 }
+        }
+        u.golfrounds[g.id]=round;
+        this.usersCollection.doc(u.uid).update(u);
+      }
+    })
+  }
+  getScore(user, currentHole) {
+    user = (user) ? user : this.currentUser;
+    if (!user) return 0;
+
+    let score = user.golfrounds[this.golfround].score;
+
+    if  (currentHole) {
+      return (score[currentHole]) ? score[currentHole] : 0
+    } else {
+      let s = 0;
+      Object.keys(score).forEach(k=> {
+        s += score[k];
+      })
+      return s;
+    } 
+  }
+  changeScore(change) {
+    if (!this.currentHole) this.currentHole = 1;
+    if (!this.currentUser.golfrounds[this.golfround].score[this.currentHole])
+    this.currentUser.golfrounds[this.golfround].score[this.currentHole]=1;
+    this.currentUser.golfrounds[this.golfround].score[this.currentHole] += change;
+    this.usersCollection.doc(this.currentUser.uid).update(this.currentUser);
+  }
+  onScroll(event) {
+    let t = event.currentTarget;
+    let c = Array.from(t.children);
+    let parentRec = t.getBoundingClientRect();
+    let guessH = 1;
+
+    c.forEach((element: any) => {
+      let rect = element.getBoundingClientRect();
+      let k={};
+      for (var key in parentRec) {
+        k[key]=Math.abs(parentRec[key]-rect[key]);
+      }
+      let h = element.getAttribute('value');
+      if (k['left'] < 500) {
+        this.currentHole = h-1;
+      }
+    });
+  }
+
+
+
+  isScrolledIntoView(el) {
+    if (!el) return false;
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return isVisible;
+  }
 
   public triggerSnapshot(): void {
     this.trigger.next();
@@ -173,60 +249,4 @@ export class HoleComponent implements OnInit {
     return this.trigger.asObservable();
   }
 
-
-  constructor(
-    db: AngularFirestore,
-    private auth: AuthService
-  ) {
-
-    console.log(JSON.stringify(this.holes))
-
-    this.users = db.collection('users').valueChanges();
-    this.getUser();
-    WebcamUtil.getAvailableVideoInputs()
-      .then((mediaDevices: MediaDeviceInfo[]) => {
-        this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
-      });
-    db.collection('games').valueChanges().subscribe(g => {
-      this.games = g;
-    });
-  }
-
-  ngOnInit() {
-  }
-
-  updateProfileImage(currentUser) {
-    currentUser.photoURL = this.webcamImage;
-    this.auth.updateUserData(currentUser).then(this.webcamImage = undefined);
-  }
-  updateDisplayName(currentUser, name) {
-    currentUser.displayName = name;
-    this.auth.updateUserData(currentUser).then(this.webcamImage = undefined);
-  }
-  updateSMS(currentUser, phoneNumber) {
-    currentUser.phoneNumber = phoneNumber;
-    this.auth.updateUserData(currentUser).then();
-  }
-  getUser() {
-    this.user = this.auth.getUser();
-  }
-  getKey(seat) {
-    let key = Object.keys(seat)[0];
-    if (!this.games) return;
-    let game = this.games.filter(game => game.id == key);
-    game[0].seat = seat[key];
-    return game;
-  }
-
-  getGame(item) {
-    let gameId = item.key;
-    let isStar = item.value
-    if (!isStar) return;
-    let game = this.games.filter(game => game.id == gameId)
-    return game;
-  }
-  switchUser(id) {
-    this.auth.switchUser(id);
-    this.getUser();
-  }
 }
