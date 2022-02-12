8602f92ffe534fd1950140f12cbb25ec1f903e83
Mauricio Lopez <mauricio.lopez.contractor@pepsico.com>
2022-02-12 07:32:30 -0500

    feb22

--- scraper/src/index copy.ts
+++ scraper/src/index copy.ts
@@ -3,23 +3,22 @@ import puppeteer, { ElementHandle } from 'puppeteer';
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.90min.com/posts/2022-mls-schedule-fixtures-results', {
+  await page.goto('https://www.atlutd.com/schedule/', {
     waitUntil: 'networkidle2'
   });
   //convert elements to json from selector '.mls-o-match-strip__club-group'
   const json = await page.evaluate(() => {
-    const elements = document.querySelectorAll('main');
+    const elements = document.querySelectorAll('.mls-o-match-strip__club-group');
     const json = Array.from(elements).map(element => {
       try {
         return {
-          // home: element.querySelector('h2')!.textContent,
-          away: element.querySelector('em')!.textContent,
-          date: element.querySelector('p')!.textContent
+          home: element.querySelector('.mls-o-match-strip__club--home')!.textContent,
+          away: element.querySelector('.mls-o-match-strip__club--away')!.textContent,
+          date: element.querySelector('.mls-o-match-strip__match-time')!.textContent
         }
       } catch (error) {
         console.log(error);
       };
-      return element.textContent
     });
     return json;
   });
